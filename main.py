bf6877ea37f0136b081796325e83cdef93ec8cb2
linda2927 <linda2927@naver.com>
2022-06-22 09:02:54 +0900

    refac: change table name from Todos to Todo

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+from typing import Optional, Union
+
 from fastapi import FastAPI, Depends, status, HTTPException
+from pydantic import BaseModel, Field
+from sqlalchemy.orm import Session
+
 from sql_app import models
 from sql_app.database import engine, SessionLocal
-from sqlalchemy.orm import Session
-from pydantic import BaseModel, Field
-from typing import Optional, Union
 
 description = """
     TODO Project API 🚀
@@ -32,19 +34,20 @@ tags_metadata = [
 ]
 
 app = FastAPI(
-            title="Todo Project API",
-            description=description,
-            version="0.0.1",
-            contact={
-                "name": "Earthly Jisoo",
-                "url": "https://github.com/linda2927",
-                "email": "linda2927@naver.com",
-            },
-            license_info={
-                "name": "Apache 2.0",
-                "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
-            },
-            openapi_tags=tags_metadata)
+    title="Todo Project API",
+    description=description,
+    version="0.0.1",
+    contact={
+        "name": "Earthly Jisoo",
+        "url": "https://github.com/linda2927",
+        "email": "linda2927@naver.com",
+    },
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },
+    openapi_tags=tags_metadata,
+)
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -62,7 +65,9 @@ class Todo(BaseModel):
     id: Optional[int]
     title: str
     description: Optional[str]
-    priority: int = Field(gt=0, lt=6, description="The priority must be between 1 and 5.")
+    priority: int = Field(
+        gt=0, lt=6, description="The priority must be between 1 and 5."
+    )
     complete: bool = Field(default=False)
 
     class Config:
@@ -82,14 +87,16 @@ def raise_404_error(detail="Todo not found."):
     return HTTPException(status_code=404, detail=detail)
 
 
-@app.get("/todos", status_code=status.HTTP_200_OK, tags=['Todos'])
+@app.get("/todos", status_code=status.HTTP_200_OK, tags=["Todos"])
 def get_all(db: Session = Depends(get_db)):
-    return db.query(models.Todos).all()
+    return db.query(models.Todo).all()
 
 
-@app.post("/todos/", status_code=status.HTTP_201_CREATED, tags=['Todos'], response_model=Todo)
+@app.post(
+    "/todos/", status_code=status.HTTP_201_CREATED, tags=["Todos"], response_model=Todo
+)
 def create_todo(todo: Todo, db: Session = Depends(get_db)):
-    new_todo = models.Todos()
+    new_todo = models.Todo()
     new_todo.title = todo.title
     new_todo.description = todo.description
     new_todo.priority = todo.priority
@@ -101,22 +108,34 @@ def create_todo(todo: Todo, db: Session = Depends(get_db)):
     return new_todo
 
 
-@app.get("/todos/{todo_id}", status_code=status.HTTP_200_OK, tags=['Todos'], response_model=Todo)
+@app.get(
+    "/todos/{todo_id}",
+    status_code=status.HTTP_200_OK,
+    tags=["Todos"],
+    response_model=Todo,
+)
 def get_todo(todo_id: int, db: Session = Depends(get_db)):
-    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+    todo_model = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
     if todo_model is not None:
         return todo_model
     raise raise_404_error()
 
 
-@app.patch("/todos/{todo_id}", status_code=status.HTTP_200_OK, tags=['Todos'], response_model=Todo)
-def update_todo(todo_id: int,
-                new_title: Union[str, None] = None,
-                new_description: Union[str, None] = None,
-                new_priority: Union[str, None] = None,
-                new_complete: Union[str, None] = None,
-                db: Session = Depends(get_db)):
-    todo = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+@app.patch(
+    "/todos/{todo_id}",
+    status_code=status.HTTP_200_OK,
+    tags=["Todos"],
+    response_model=Todo,
+)
+def update_todo(
+    todo_id: int,
+    new_title: Union[str, None] = None,
+    new_description: Union[str, None] = None,
+    new_priority: Union[str, None] = None,
+    new_complete: Union[str, None] = None,
+    db: Session = Depends(get_db),
+):
+    todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
     if todo is None:
         raise raise_404_error()
 
@@ -131,26 +150,22 @@ def update_todo(todo_id: int,
 
     db.commit()
 
-    return {
-        'status': 200,
-        'transaction': 'Successful'
-    }
+    return {"status": 200, "transaction": "Successful"}
 
 
-@app.delete("/todos/{todo_id}",
-            status_code=status.HTTP_200_OK,
-            tags=['Todos'],
-            responses={200: {"description": "Successfully deleted."}})
+@app.delete(
+    "/todos/{todo_id}",
+    status_code=status.HTTP_200_OK,
+    tags=["Todos"],
+    responses={200: {"description": "Successfully deleted."}},
+)
 def delete_todo(todo_id: int, db: Session = Depends(get_db)):
-    todo = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+    todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
     if todo is None:
         raise raise_404_error()
 
-    db.query(models.Todos).filter(models.Todos.id == todo_id).delete()
+    db.query(models.Todo).filter(models.Todo.id == todo_id).delete()
 
     db.commit()
 
-    return {
-        'status': 200,
-        'transaction': 'Successful'
-    }
+    return {"status": 200, "transaction": "Successful"}
