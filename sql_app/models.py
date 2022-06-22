a1bb64b54e544f8dad56585f42c1e1663dca2e06
JS <linda2927@naver.com>
2022-06-22 09:45:44 +0900

    Auto stash before merge of "master" and "origin/refac/todo-table"

--- sql_app/models.py
+++ sql_app/models.py
@@ -1,12 +1,30 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy import Boolean, Column, Integer, String, ForeignKey
 from .database import Base
 
 
-class Todos(Base):
-    __tablename__ = 'todos'
+class Todo(Base):
+    __tablename__ = "todo"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    description = Column(String)
+    title = Column(String(200))
+    description = Column(String(500))
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("user.id"))
+
+    owner = relationship("User", back_populates="todos")
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(100), unique=True, index=True)
+    username = Column(String(30), unique=True, index=True)
+    first_name = Column(String(30))
+    last_name = Column(String(30))
+    hashed_password = Column(String(50))
+    is_active = Column(Boolean, default=True)
+
+    todos = relationship("Todo", back_populates="owner")
