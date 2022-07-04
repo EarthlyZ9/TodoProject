577d2ca5a9c204bd880fdc0266607ae4fe2fed16
linda2927 <linda2927@naver.com>
2022-07-04 12:53:52 +0900

    refac: separate crud fuction for todos routes

--- app/api/api_v1/endpoints/todos.py
+++ app/api/api_v1/endpoints/todos.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter, Depends, status
+from typing import List
+
+from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 
-from app.api.deps import get_current_user, get_db
+from app import crud
+from app.api.deps import get_current_user, get_db, get_current_admin
 from app.dependencies import raise_404_error, get_authorization_exception
-from app.models.todo import Todo
 from app.models.user import User
 from app.schemas import todo_schema
 
@@ -19,10 +21,13 @@ router = APIRouter(
     status_code=status.HTTP_200_OK,
     tags=["Todos"],
     summary="Get all todos. Only for administrators.",
-    operation_id="get_all_admin",
+    operation_id="read_all",
 )
-def get_all_admin(db: Session = Depends(get_db)):
-    return db.query(Todo).all()
+def read_all(
+    db: Session = Depends(get_db), current_user: User = Depends(get_current_admin)
+):
+    if current_user:
+        return crud.todo.get_multi(db)
 
 
 @router.post(
@@ -37,17 +42,19 @@ def create_todo(
     db: Session = Depends(get_db),
     current_user: User = Depends(get_current_user),
 ):
-    new_todo = Todo(**todo.dict(), owner_id=current_user.id)
-    db.add(new_todo)
-    db.commit()
-
-    return new_todo
+    return crud.todo.create_with_owner(db=db, obj_in=todo, owner_id=current_user.id)
 
 
 @router.get(
-    "/", summary="Get all todos of current user.", status_code=status.HTTP_200_OK
+    "/",
+    summary="Get all todos of current user.",
+    status_code=status.HTTP_200_OK,
+    operation_id="read_todos",
+    response_model=List[todo_schema.TodoOut],
 )
-def get_todos(current_user: User = Depends(get_current_user)):
+def read_todos(current_user: User = Depends(get_current_user)):
+    if current_user.todos is None:
+        return HTTPException(status_code=status.HTTP_200_OK, detail="No todos created.")
     return current_user.todos
 
 
@@ -63,15 +70,12 @@ def get_todo_by_id(
     db: Session = Depends(get_db),
     current_user: User = Depends(get_current_user),
 ):
-    todo_model = (
-        db.query(Todo)
-        .filter(Todo.id == todo_id)
-        .filter(Todo.owner_id == current_user.id)
-        .first()
-    )
-    if todo_model is not None:
-        return todo_model
-    raise raise_404_error(detail="Cannot find todo for the provided id.")
+    todo = crud.todo.get(db=db, id=todo_id)
+    if todo is None:
+        raise raise_404_error(detail="Cannot find todo for the provided id.")
+    if todo.owner_id == current_user.id:
+        return todo
+    raise get_authorization_exception()
 
 
 @router.patch(
@@ -80,34 +84,20 @@ def get_todo_by_id(
     summary="Update current user's todo by id.",
     operation_id="update_todo",
     response_model=todo_schema.TodoOut,
+    responses={200: {"description": "Successfully updated."}},
 )
 def update_todo(
     todo_id: int,
-    todo: todo_schema.TodoCreate,
+    todo: todo_schema.TodoUpdate,
     db: Session = Depends(get_db),
     current_user: User = Depends(get_current_user),
 ):
-    todo_by_id = db.query(Todo).filter(Todo.id == todo_id).first()
+    todo_by_id = crud.todo.get(db=db, id=todo_id)
     if todo_by_id is None:
-        raise raise_404_error()
-    else:
-        todo_by_id = (
-            db.query(Todo)
-            .filter(Todo.id == todo_id)
-            .filter(Todo.owner_id == current_user.id)
-            .first()
-        )
-        if todo_by_id is None:
-            raise get_authorization_exception()
-
-    todo_data = todo.dict(exclude_unset=True)
-    for key, value in todo_data.items():
-        setattr(todo_by_id, key, value)
-    db.add(todo_by_id)
-    db.commit()
-    db.refresh(todo_by_id)
-
-    return todo_by_id
+        raise raise_404_error(detail="Cannot find todo for the provided id.")
+    if todo_by_id.owner_id == current_user.id:
+        return crud.todo.update(db=db, db_obj=todo_by_id, obj_in=todo)
+    raise get_authorization_exception()
 
 
 @router.delete(
@@ -116,27 +106,16 @@ def update_todo(
     summary="Delete current user's todo by id.",
     operation_id="delete_todo",
     responses={200: {"description": "Successfully deleted."}},
+    response_model=todo_schema.TodoOut,
 )
 def delete_todo(
     todo_id: int,
     db: Session = Depends(get_db),
     current_user: User = Depends(get_current_user),
 ):
-    todo = db.query(Todo).filter(Todo.id == todo_id).first()
+    todo = crud.todo.get(db=db, id=todo_id)
     if todo is None:
         raise raise_404_error("Cannot find todo for the provided id.")
-    else:
-        todo = (
-            db.query(Todo)
-            .filter(Todo.id == todo_id)
-            .filter(Todo.owner_id == current_user.id)
-            .first()
-        )
-        if todo is None:
-            raise get_authorization_exception()
-
-    db.query(Todo).filter(Todo.id == todo_id).delete()
-
-    db.commit()
-
-    return {"status": 200, "transaction": "Successful"}
+    if todo.owner_id == current_user.id:
+        return crud.todo.remove(db=db, id=todo_id)
+    raise get_authorization_exception()
--- app/crud/__init__.py
+++ app/crud/__init__.py
@@ -1,1 +1,2 @@
+from .crud_todo import todo
 from .crud_user import user
