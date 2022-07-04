40b5955e395db7708c0bff5593d3a861f174898b
linda2927 <linda2927@naver.com>
2022-07-04 13:25:07 +0900

    refac: separate crud fuction for address routes

--- app/api/api_v1/endpoints/address.py
+++ app/api/api_v1/endpoints/address.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 
+from app import crud
 from app.api.deps import get_current_user, get_db
-from app.dependencies import raise_404_error
+from app.dependencies import raise_404_error, get_authorization_exception
 from app.models.address import Address
 from app.models.user import User
 from app.schemas import address_schema, address_user_schema, user_schema
@@ -21,11 +22,18 @@ router = APIRouter(
     response_model=address_user_schema.AddressWithUser,
     operation_id="get_address_by_id",
 )
-def get_address_by_id(address_id: int, db: Session = Depends(get_db)):
-    address = db.query(Address).filter(Address.id == address_id).first()
+def get_address_by_id(
+    address_id: int,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user),
+):
+    address = crud.address.get(db=db, id=address_id)
     if address is None:
         raise raise_404_error(detail="Cannot find address for the provided id.")
-    return address
+    if address.user.id == current_user.id or crud.user.is_admin(current_user):
+        return address
+
+    raise get_authorization_exception()
 
 
 @router.get(
@@ -58,14 +66,9 @@ def update_address(
 ):
     if current_user.address is None:
         raise HTTPException(status_code=status.HTTP_200_OK, detail="No address yet.")
-    address = db.query(Address).filter(Address.id == current_user.address_id).first()
-    address_data = new_address.dict(exclude_unset=True)
-    for key, value in address_data.items():
-        setattr(address, key, value)
-    db.add(address)
-    db.commit()
-    db.refresh(address)
-    return address
+    address = crud.address.get(db=db, id=current_user.address_id)
+
+    return crud.address.update(db=db, db_obj=address, obj_in=new_address)
 
 
 @router.delete(
@@ -84,10 +87,7 @@ def delete_address(
 ):
     if current_user.address is None:
         raise raise_404_error(detail="No address to delete.")
-    address = db.query(Address).filter(Address.id == current_user.address_id).first()
-    db.delete(address)
-    db.commit()
-    return db.query(User).filter(User.id == current_user.id).first()
+    return crud.address.remove(db=db, id=current_user.address_id)
 
 
 @router.post(
@@ -95,20 +95,19 @@ def delete_address(
     status_code=status.HTTP_200_OK,
     summary="Add an address for user",
     response_model=address_user_schema.AddressWithUser,
+    operation_id="create_address",
 )
 def create_address(
-    address: address_schema.AddressCreate,
+    address_in: address_schema.AddressCreate,
     db: Session = Depends(get_db),
     current_user: User = Depends(get_current_user),
 ):
-    address_model = Address(**address.dict())
+    address = crud.address.create(db=db, obj_in=address_in)
 
-    db.add(address_model)
-    db.flush()  # returns id
+    user = crud.user.get(db=db, id=current_user.id)
+    user.address_id = address.id
 
-    user = db.query(User).filter(User.id == current_user.id).first()
-    user.address_id = address_model.id
     db.add(user)
-
     db.commit()
-    return address_model
+
+    return address
